d246662e93fe5f6d356f2f3c55c18d461b9ce061
DuyBui187 <63256014+DuyBui187@users.noreply.github.com>
2025-09-04 17:38:48 +0700

    fix: Adjust face animation during toothbrushing

--- Assets/Scripts/Managers/RoomManager.cs
+++ Assets/Scripts/Managers/RoomManager.cs
@@ -38,7 +38,7 @@ public class RoomManager : SingletonMonobehaviour<RoomManager>
     private void Start()
     {
         player = PlayerControl.Instance;
-        InitRoom(RoomType.Danbi);
+        InitRoom(RoomType.WC_F1);
     }
 
     // ─────────────────────────────────────────────────────
